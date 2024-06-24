78a2e65b2f3c2bdce333033833e1c931527edfa4
vaishu3015 <118594297+vaishu3015@users.noreply.github.com>
2024-06-24 18:48:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("centered_nav");
+    if (x.className === "rc_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "rc_nav";
+    }
+}
+
